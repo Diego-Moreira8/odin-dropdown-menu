e2301a43d7cd166d8a4c5fb56e2b2875aca70d7d
Diego <diego.moreira529@gmail.com>
2022-11-10 21:44:15 -0300

    New header

--- index.js
+++
@@ -1,9 +0,0 @@
-document.querySelectorAll(".menu").forEach((menu) => {
-  menu.addEventListener("mouseenter", () => {
-    const dropdownMenu = menu.parentElement.querySelector(".dropdown-menu");
-    dropdownMenu.classList.add("visible");
-    dropdownMenu.addEventListener("mouseleave", () => {
-      dropdownMenu.classList.remove("visible");
-    });
-  });
-});
